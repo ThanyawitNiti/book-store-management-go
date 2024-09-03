b1e8e0f4b0181aa913b102092197c064fc84ebc3
Palm-Thanyawit <thanyawit.nitipornwattana@gmail.com>
2024-09-04 00:34:39 +0700

    create : crud func at book.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,2 +1,19 @@
-//flow
-//user => routers => controllers(logic) => db => reside models
+// flow
+// user => routers => controllers(logic) => db => reside models
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/ThanyawitNiti/book-store-management-go/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
