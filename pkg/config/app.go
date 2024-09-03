dcfa1021d7171cb598cc07f504a874ccca7bcbeb
Palm-Thanyawit <thanyawit.nitipornwattana@gmail.com>
2024-09-04 02:14:40 +0700

    problem : getting .env

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,8 +1,14 @@
 package config
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -10,7 +16,26 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:Thanyawit/book_store_go?charset=utf8&parseTime=True&loc=Local")
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	//use ../.env because main.go inside /cmd
+	err = godotenv.Load(filepath.Join(pwd, "../../pkg/config/.env"))
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	fmt.Printf("godotenv : %s = %s \n", "dbUser", dbUser)
+	fmt.Printf("godotenv : %s = %s \n", "DB Host", dbHost)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+	d, err := gorm.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
 	}
